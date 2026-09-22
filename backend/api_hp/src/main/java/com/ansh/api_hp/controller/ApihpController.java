baee50f5c02f353efb5b61cd86109fe13b9d398f
Ansh <anshverma5974@gamil.com>
2026-09-23 02:29:59 +0530

    Docker updation

--- backend/api_hp/src/main/java/com/ansh/api_hp/controller/ApihpController.java
+++ backend/api_hp/src/main/java/com/ansh/api_hp/controller/ApihpController.java
@@ -11,6 +11,10 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = {
+        "http://localhost:5173",
+        "https://api-health-analyzer.vercel.app"
+})
 @RequestMapping("/api/monitors")
 public class ApihpController {
 
